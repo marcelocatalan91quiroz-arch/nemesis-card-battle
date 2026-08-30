32ba0f24134bf13271b3d3b9809680365a8917dd
marcelocatalan91quiroz-arch <marcelocatalan.91.quiroz@gmail.com>
2026-08-30 04:25:58 +0200

    refactor: require full Duel Master art set

--- js/card-art/card-art-registry.js
+++ js/card-art/card-art-registry.js
@@ -13,7 +13,7 @@
  const required=[
    ...Array.from({length:20},(_,i)=>'IDR-'+String(i+1).padStart(3,'0')),
    ...Array.from({length:20},(_,i)=>'MGR-'+String(i+1).padStart(3,'0')),
-   ...Array.from({length:10},(_,i)=>'DM-'+String(i+11).padStart(3,'0')),
+   ...Array.from({length:20},(_,i)=>'DM-'+String(i+1).padStart(3,'0')),
    'TN-MAG-001','TN-MAG-002','TN-ARM-001','TN-ARM-002','TN-TRP-001'
  ];
  window.NEMESIS_CARD_ART_AUDIT=()=>({
