d738989e6ccdd834538ddf0efd1bb50d2d87b971
marcelocatalan91quiroz-arch <marcelocatalan.91.quiroz@gmail.com>
2026-08-31 12:42:22 +0200

    test: release audita Eclipse Online

--- tests/release-readiness.test.cjs
+++ tests/release-readiness.test.cjs
@@ -65,6 +65,7 @@ A('online 2 jugadores, turnos y reconexión',api.includes('ROOM_FULL')&&api.incl
 A('online Duel Master 20 efectos',api.includes('DM_EFFECT_HANDLERS')&&api.includes("'DM-020'"));
 A('online Olimpo 11/11 autoritativo',api.includes('OLIMPO_CATALOG')&&api.includes('OLIMPO:OLIMPO_DECK')&&api.includes('resolveOlimpoAbility')&&api.includes('resolveOlimpoSupport'));
 A('online Caballeros 20/20 autoritativo',api.includes('CS_CATALOG')&&api.includes('CABALLEROS_SUBMUNDO:CABALLEROS_SUBMUNDO_DECK')&&api.includes('resolveCsAbility')&&api.includes('resolveCsSupport'));
+A('online Eclipse MS-001 motor preparado y propiedad protegida',api.includes('resolveAbsoluteEclipseSupport')&&api.includes('NEMESIS_ECLIPSE_POWERS')&&api.includes('ONLINE_REDEEM_PROOF_REQUIRED'));
 A('online soporta armaduras reales',api.includes("equipment:{weapon:null,armor:null,relic:null}")&&api.includes("['weapon','armor','relic']"));
 
 if(process.exitCode)process.exit(process.exitCode);
